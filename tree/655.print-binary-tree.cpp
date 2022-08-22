08a8a500ccb82df1c5b1ed72593cd5b6b050c573
tuhooo <tuhooo@163.com>
2022-08-22 23:20:51 +0800

    早点睡

--- tree/655.print-binary-tree.cpp
+++ tree/655.print-binary-tree.cpp
@@ -52,7 +52,52 @@ struct TreeNode {
 
 class Solution {
 public:
+	
+	// 平方求幂
+	int ipow(int base, int exp) {
+		int result = 1;
+		for (;;) {
+			if (exp & 1)
+				result *= base;
+			exp >>= 1;
+			if (!exp)
+				break;
+			base *= base;
+		}
+
+		return result;
+	}
+
+	int getLevel(TreeNode* root) {
+		return 2;
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="root">当前根节点</param>
+	/// <param name="currentX">根节点的X坐标</param>
+	/// <param name="currentLevel">当前的层数</param>
+	/// <param name="totalLevel">总的层数</param>
+	/// <param name="res">记录结果的矩阵</param>
+	void recursive(TreeNode* root, int currentX, int currentLevel, int totalLevel, vector<vector<string>>* res) {
+
+	}
+
+
+
 	vector<vector<string>> printTree(TreeNode* root) {
+
+		// 计算整棵树的层数
+		int level = getLevel(root);
+
+		int full_node_num = ipow(2, level) - 1;
+
+		// 创建矩阵
+		vector<vector<string>> res(level, vector<string>(full_node_num, ""));
+
+		recursive(root, full_node_num, 1, &res);
+
 
 	}
 };
