9bb915d28bd5c933bb65670c3bc2724cb97d5c5b
duhbbx <duhbbx@gmail.com>
2022-09-15 22:39:55 +0800

    感觉自己写kmp还是有点蛋疼，效率也不高

--- 未分类/28. 找出字符串中第一个匹配项的下标.cpp
+++ 未分类/28. 找出字符串中第一个匹配项的下标.cpp
@@ -15,8 +15,6 @@
 using namespace std;
 
 
-
-
 template<typename T>
 void print(T t) {
 
@@ -42,7 +40,46 @@ void print(T t) {
 
 class Solution {
 public:
+
+    vector<int> getNext(const string &needle) {
+
+        int n = needle.length();
+        vector<int> next(n, 0);
+
+
+        // [0, i] 就是当前要判断的字符串的长度
+        for (int i = 1; i < n; ++i) {
+            cout << "当前判断的字符串为: " << needle.substr(0, i + 1) << endl;
+
+            for (int j = 1; j <= i; ++j) {
+                int tmp = 0;
+                cout << "前缀: " << needle.substr(0, j) << ", 后缀：" << needle.substr(i + 1 - j, j) << endl;
+
+                for (int k = 0; k < j; ++k) {
+                    if (needle[k] == needle[i + 1 - j + k]) {
+                        tmp++;
+                    } else {
+                        break;
+                    }
+                }
+
+                if (tmp > next[i]) {
+                    next[i] = tmp;
+                }
+
+            }
+
+        }
+
+
+        print(next);
+
+        return next;
+    }
+
     int strStr(string haystack, string needle) {
+
+        if (needle.length() == 0) return -1;
 
 
         return -1;
@@ -59,12 +96,9 @@ public:
 void printCurrentFileName() {
     string file = __FILE__;
     int pos = file.find_last_of("/");
-    string fileName = pos == -1 ? file : file.substr(pos+1);
+    string fileName = pos == -1 ? file : file.substr(pos + 1);
     cout << "\n【当前题目为：" << fileName << "】" << endl;
 }
-
-
-
 
 
 int main() {
@@ -74,6 +108,8 @@ int main() {
 
 
     Solution solution;
+
+    solution.getNext("ababaca");
 
 
     /*print<vector<int>>({ 1, 2, 3, 4 });*/
