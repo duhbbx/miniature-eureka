b0e88c29a81586f9f674171cc0e281f16fbd3ada
duhbbx <duhbbx@gmail.com>
2022-09-15 20:55:04 +0800

    又写了几个

---
+++ 双指针/344. 反转字符串.cpp
@@ -0,0 +1,92 @@
+//
+// Created by duhbb on 2022/9/15.
+//
+#include <iostream>              // 输入输出
+#include <vector>                // 可变长度数组
+#include <unordered_map>         // hashmap
+#include <stack>                 // 栈
+#include <string>                // 字符串
+#include <queue>                 // 队列
+#include <climits>               // 极限值
+#include <algorithm>             // 算法相关的
+#include <set>                   // 集合
+#include "../0000 API 模板 类/TreeNode.h"
+#include "../0000 API 模板 类/ListNode.h"
+
+
+using namespace std;
+
+
+
+
+template<typename T>
+void print(T t) {
+
+
+    if (t.empty()) {
+        cout << "容器为空............" << endl;
+        return;
+    }
+
+
+    for (typename T::const_iterator it = t.begin(); it != t.end() - 1; ++it) {
+        cout << *it << ", ";
+    }
+
+    cout << *(t.end() - 1) << endl;
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+/// 这里放OJ的类
+
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+
+        int i = 0;
+        int j = s.size() - 1;
+
+        while(i < j) {
+            swap(s[i], s[j]);
+            ++i;
+            --j;
+        }
+
+    }
+};
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+void printCurrentFileName() {
+    string file = __FILE__;
+    int pos = file.find_last_of("/");
+    string fileName = pos == -1 ? file : file.substr(pos+1);
+    cout << "\n【当前题目为：" << fileName << "】" << endl;
+}
+
+
+
+
+
+int main() {
+
+
+    printCurrentFileName();
+
+
+    Solution solution;
+
+
+    /*print<vector<int>>({ 1, 2, 3, 4 });*/
+
+    return 0;
+}
---
+++ 未分类/231. 2 的幂.cpp
@@ -0,0 +1,86 @@
+//
+// Created by duhbb on 2022/9/15.
+//
+#include <iostream>              // 输入输出
+#include <vector>                // 可变长度数组
+#include <unordered_map>         // hashmap
+#include <stack>                 // 栈
+#include <string>                // 字符串
+#include <queue>                 // 队列
+#include <climits>               // 极限值
+#include <algorithm>             // 算法相关的
+#include <set>                   // 集合
+#include "../0000 API 模板 类/TreeNode.h"
+#include "../0000 API 模板 类/ListNode.h"
+
+
+using namespace std;
+
+
+
+
+template<typename T>
+void print(T t) {
+
+
+    if (t.empty()) {
+        cout << "容器为空............" << endl;
+        return;
+    }
+
+
+    for (typename T::const_iterator it = t.begin(); it != t.end() - 1; ++it) {
+        cout << *it << ", ";
+    }
+
+    cout << *(t.end() - 1) << endl;
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+/// 这里放OJ的类
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n == 1) return true;
+        return n % 2 == 0 && (n & INT_MAX) == n;
+    }
+};
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+void printCurrentFileName() {
+    string file = __FILE__;
+    int pos = file.find_last_of("/");
+    string fileName = pos == -1 ? file : file.substr(pos+1);
+    cout << "\n【当前题目为：" << fileName << "】" << endl;
+}
+
+
+
+
+
+int main() {
+
+
+    const int& a = 10;
+
+    const int* p = &a;
+
+    printCurrentFileName();
+
+
+    Solution solution;
+
+
+    /*print<vector<int>>({ 1, 2, 3, 4 });*/
+
+    return 0;
+}
