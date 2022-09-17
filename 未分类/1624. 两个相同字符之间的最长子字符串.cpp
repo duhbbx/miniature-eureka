847253072a1329f264c403a56f2b0d19eedf3669
duhbbx <duhbbx@gmail.com>
2022-09-17 08:48:31 +0800

    这个简单哦

---
+++ 未分类/1624. 两个相同字符之间的最长子字符串.cpp
@@ -0,0 +1,101 @@
+//
+// Created by duhbb on 2022/9/17.
+//
+#include <iostream>              // 输入输出
+#include <vector>                // 可变长度数组
+#include <unordered_map>         // hashmap
+#include <stack>                 // 栈
+#include <string>                // 字符串
+#include <queue>                 // 队列
+#include <climits>               // 极限值
+#include <algorithm>             // 算法相关的
+#include <set>                   // 集合
+#include "../0000 API 模板 类/TreeNode.h"
+#include "../0000 API 模板 类/ListNode.h"
+
+
+using namespace std;
+
+
+
+
+template<typename T>
+void print(T t) {
+
+
+    if (t.empty()) {
+        cout << "容器为空............" << endl;
+        return;
+    }
+
+
+    for (typename T::const_iterator it = t.begin(); it != t.end() - 1; ++it) {
+        cout << *it << ", ";
+    }
+
+    cout << *(t.end() - 1) << endl;
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+/// 这里放OJ的类
+
+class Solution {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
+
+        int n = s.length();
+
+        vector<int> first(26, -1);
+        vector<int> second(26, -1);
+
+        int res = -1;
+        for (int i = 0; i < n; i++) {
+            int index = s[i] - 'a';
+            if (first[index] == -1) {
+                first[index] = i;
+            } else {
+                second[index] = i;
+                if (second[index] - first[index] - 1 > res) {
+                    res = second[index] - first[index] - 1;
+                }
+            }
+        }
+
+
+        return res;
+
+    }
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+void printCurrentFileName() {
+    string file = __FILE__;
+    int pos = file.find_last_of("/");
+    string fileName = pos == -1 ? file : file.substr(pos+1);
+    cout << "\n【当前题目为：" << fileName << "】" << endl;
+}
+
+
+
+
+
+int main() {
+
+
+    printCurrentFileName();
+
+
+    Solution solution;
+
+
+    /*print<vector<int>>({ 1, 2, 3, 4 });*/
+
+    return 0;
+}
