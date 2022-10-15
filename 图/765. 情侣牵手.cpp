4cfe29873b7918685373d839302628005d19568d
duhbbx <duhbbx@gmail.com>
2022-10-16 07:42:14 +0800

    情侣牵手, 连通分量

---
+++ 图/765. 情侣牵手.cpp
@@ -0,0 +1,123 @@
+#include <iostream>              // 输入输出
+#include <vector>                // 可变长度数组
+#include <unordered_map>         // hashmap
+#include <unordered_set>         // 无序集合
+#include <stack>                 // 栈
+#include <string>                // 字符串
+#include <queue>                 // 队列
+#include <climits>               // 极限值
+#include <algorithm>             // 算法相关的
+#include <set>                   // 集合
+#include "../0000 API 模板 类/TreeNode.h"
+#include "../0000 API 模板 类/ListNode.h"
+#include <numeric>
+#include <map>
+#include <cstring>
+
+
+using namespace std;
+
+
+template<typename T>
+void print(T t) {
+
+
+    if (t.empty()) {
+        cout << "容器为空............" << endl;
+        return;
+    }
+
+
+    for (typename T::const_iterator it = t.begin(); it != t.end() - 1; ++it) {
+        cout << *it << ", ";
+    }
+
+    cout << *(t.end() - 1) << endl;
+}
+
+void printCurrentFileName() {
+    string file = __FILE__;
+    int pos = file.find_last_of("/");
+    string fileName = pos == -1 ? file : file.substr(pos + 1);
+    cout << "\n【当前题目为：" << fileName << "】" << endl;
+}
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+/// 这里放OJ的类
+
+
+#define MAXN 100
+
+
+class Solution {
+public:
+
+    int union_set[MAXN];
+    int cnt;
+
+    inline void init(int n) {
+        for (int i = 0; i < n; ++i) {
+            union_set[i] = i;
+        }
+        cnt = n;
+    }
+
+    int find(int x) {
+        return x == union_set[x] ? x : (union_set[x] = find(union_set[x]));
+    }
+
+    inline void merge(int x, int y) {
+
+        int p_x = find(x), p_y = find(y);
+        if (p_x != p_y) {
+            union_set[p_x] = p_y;
+            --cnt;
+        }
+    }
+
+    int minSwapsCouples(vector<int> &row) {
+
+        init(row.size());
+        int num = row.size() >> 1;
+        for (int i = 0; i < num; ++i) {
+            merge(row[i*2], row[i*2+1]);
+
+            if (row[i*2] % 2 == 0) {
+                merge(row[i*2], row[i*2] + 1);
+            } else {
+                merge(row[i*2], row[i*2] - 1);
+            }
+
+            if (row[i*2+1] % 2 == 0) {
+                merge(row[i*2+1], row[i*2+1] + 1);
+            } else {
+                merge(row[i*2+1], row[i*2+1] - 1);
+            }
+        }
+
+        return row.size() / 2  - cnt;
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+int main() {
+
+
+    printCurrentFileName();
+
+
+    Solution solution;
+
+    vector<int> row = {0, 2, 1, 3};
+
+    solution.minSwapsCouples(row);
+
+
+    /*print<vector<int>>({ 1, 2, 3, 4 });*/
+
+    return 0;
+}
