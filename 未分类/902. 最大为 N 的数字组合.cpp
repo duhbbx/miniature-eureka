672231c89cb8bdf838481d49193c6602afc9ee04
duhbbx <duhbbx@gmail.com>
2022-10-18 07:46:12 +0800

    902 最大为n的数字组合

---
+++ 未分类/902. 最大为 N 的数字组合.cpp
@@ -0,0 +1,80 @@
+//
+// Created by tuhooo on 2022/10/18.
+//
+
+#include <iostream>              // 输入输出
+#include <vector>                // 可变长度数组
+#include <unordered_map>         // hashmap
+#include <unordered_set>         // 无序集合
+#include <stack>                 // 栈
+#include <string>                // 字符串
+#include <queue>                 // 队列
+#include <climits>               // 极限值
+#include <algorithm>             // 算法相关的
+#include <set>                   // 集合
+#include "../0000 API 模板 类/TreeNode.h"
+#include "../0000 API 模板 类/ListNode.h"
+#include <numeric>
+#include <map>
+#include <cstring>
+
+
+using namespace std;
+
+
+
+
+template<typename T>
+void print(T t) {
+
+
+    if (t.empty()) {
+        cout << "容器为空............" << endl;
+        return;
+    }
+
+
+    for (typename T::const_iterator it = t.begin(); it != t.end() - 1; ++it) {
+        cout << *it << ", ";
+    }
+
+    cout << *(t.end() - 1) << endl;
+}
+
+void printCurrentFileName() {
+    string file = __FILE__;
+    int pos = file.find_last_of("/");
+    string fileName = pos == -1 ? file : file.substr(pos+1);
+    cout << "\n【当前题目为：" << fileName << "】" << endl;
+}
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+/// 这里放OJ的类
+
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+
+
+        return {};
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+int main() {
+
+
+    printCurrentFileName();
+
+
+    Solution solution;
+
+
+    /*print<vector<int>>({ 1, 2, 3, 4 });*/
+
+    return 0;
+}
