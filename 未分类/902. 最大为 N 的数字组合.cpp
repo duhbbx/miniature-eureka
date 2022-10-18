8762b5d2a2ac4a0059e93e8566a4573bbb5bc500
duhbbx <duhbbx@gmail.com>
2022-10-18 13:53:24 +0800

    902 最大为n的数字组合, 回溯求最大值中....

--- 未分类/902. 最大为 N 的数字组合.cpp
+++ 未分类/902. 最大为 N 的数字组合.cpp
@@ -22,8 +22,6 @@
 using namespace std;
 
 
-
-
 template<typename T>
 void print(T t) {
 
@@ -44,7 +42,7 @@ void print(T t) {
 void printCurrentFileName() {
     string file = __FILE__;
     int pos = file.find_last_of("/");
-    string fileName = pos == -1 ? file : file.substr(pos+1);
+    string fileName = pos == -1 ? file : file.substr(pos + 1);
     cout << "\n【当前题目为：" << fileName << "】" << endl;
 }
 
@@ -57,25 +55,86 @@ void printCurrentFileName() {
 
 class Solution {
 public:
-    int atMostNGivenDigitSet(vector<string>& digits, int n) {
+    int atMostNGivenDigitSet(const vector<string> &digits, int n) {
 
         vector<int> bits_of_n;
-        while(n) {
+        while (n) {
             int rem = n % 10;
             bits_of_n.push_back(rem);
             n = (n - rem) / 10;
         }
 
-        // 找到小于给定数的最大数
+        int bits_num = bits_of_n.size();
+
+        vector<char> biggest(bits_num, ' ');
 
 
-        // 对找到的数计算总的组合数
+        const auto &dfs = [&](auto &&self, int first, int start,
+                              int digits_index) -> void {
+            if (first == -1) {  // 如果已经遍历到了最后一位则退出
+                return;
+            }
+
+
+            if (first == start) {   // 如果是第一位的话, 可能存在轮空的情况
+
+                int j = digits_index;
+                for (; j >= 0; --j) {
+
+                    if (digits[j][0] - '0' <= bits_of_n[first]) {    // 选择小于 n 的当前位的最大的一个数字
+                        biggest[first] = digits[j][0];
+                        digits_index = j;   // 记住此位的当前位置
+                        break;
+                    }
+                }
+
+                if (j == -1) {
+                    biggest[first] = ' ';
+                }
+
+                self(self, first-1, start, digits.size()-1);
+
+            } else {
+                if (biggest[first + 1] == ' ') {    // 如果前一位轮空了
+
+                    for (int k = first; k >= 0; --k) {
+                        biggest[k] = digits[digits.size()-1][0];
+                    }
+
+                    self(self, -1, -1, -1);
+
+                } else {
+
+                    int j = digits_index;
+                    for (; j >= 0; --j) {
+                        if (digits[j][0] - '0' <= bits_of_n[first]) {    // 选择小于 n 的当前位的最大的一个数字
+                            biggest[first] = digits[j][0];
+                            digits_index = j;   // 记住此位的当前位置
+                            self(self, first-1, start, digits.size()-1);
+                            break;
+                        }
+                    }
+
+                    if (j == -1) {
+                        self(self, first + 1, start, digits_index - 1);
+                    }
+                }
+            }
+        };
+
+        dfs(dfs, bits_num - 1, bits_num - 1, digits.size()-1);
+
+        print(biggest);
 
 
 
         return 0;
+    };
 
-    }
+
+
+
+
 };
 
 
@@ -88,6 +147,8 @@ int main() {
 
 
     Solution solution;
+
+    solution.atMostNGivenDigitSet({"3", "5", "7"}, 3000);
 
 
     /*print<vector<int>>({ 1, 2, 3, 4 });*/
