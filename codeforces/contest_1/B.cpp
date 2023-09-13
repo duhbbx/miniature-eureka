c6a7827a11fad4d8097a2cecd3f01088df09cb4c
duhbb <duhbbx@gmail.com>
2023-09-13 23:12:01 +0800

    需要判断a，b都是字符串或者数字

--- codeforces/contest_1/B.cpp
+++ codeforces/contest_1/B.cpp
@@ -1,25 +1,80 @@
-
-
+#include <cstdio>
 #include <iostream>
-#include <sstream>
+#include <string>
 
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
+#define CLEAN_BUFFER std::cin.ignore(100, '\n')
 
+#define INIT_IO                                 \
+    do {                                        \
+        ios::sync_with_stdio(false);            \
+        std::cin.tie(0);                        \
+        std::cout.tie(0);                       \
+    } while (0)
+
+
+struct numeration {
+    int position[4];
+    int type;
+};
+
+
+bool same_type(char a, char b) {
+    
+
+}
+
+struct numeration get(const std::string num) {
+    struct numeration instance;
+
+    instance.position[0] = 0;
+    instance.position[1] = 0;
+    instance.position[2] = 0;
+    instance.position[3] = 0;
+
+    instance.type = 0;
+
+
+    int j = 1;
+    for (int i = 1; i < num.length(); ++i) {
+        if (num[i-1] != num[i]) {
+            instance.position[j++] = i;
+        }
+    }
+
+    if (j == 2) {
+        instance.type = 1;
+    } else {
+        instance.type = 2;
+    }
+
+    cout << "字符串的类型为: " << instance.type << endl;
+
+    return instance;
+}
+
+
+
+int main() {
+
+    INIT_IO;
 
     int n;
-
     cin >> n;
 
-    std::string inputString;
+    CLEAN_BUFFER;
 
 
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    std::getline(std::cin, inputString);
 
-    cout << "您输入了: " << inputString;
+    while (n--) {
+        string s;
+        getline(cin, s);
+        cout << "当前输入的字符串: " << s << std::endl;
+
+        get(s);
+        
+    }
+
+    return 0;
 }
